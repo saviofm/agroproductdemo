9f2d498ba4fcd7a0a3aa23610832b34bdf6b5dd7
Savio Marques <savio.marques@sap.com>
2022-07-07 20:01:42 -0300

    AdjustModules

--- srv/cat-service/cat-service.js
+++ srv/cat-service/cat-service.js
@@ -1,9 +1,6 @@
 const cds = require('@sap/cds');
 const xsenv = require("@sap/xsenv");
-const { getBundle } = require('./common/i18n');
 
-
-const AWS = require('aws-sdk')
 
 class CatalogService extends cds.ApplicationService {
     init() {
