4ec0f90f49c40a665a2f15ba0aee1f73bdb84eff
Savio Marques <savio.marques@sap.com>
2022-07-08 01:07:22 +0000

    App Produtos

---
+++ app/produtos/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("agroproductdemo.produtos.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
