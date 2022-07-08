3330abeb673e73be5e9fd022f43cdd75b1292325
Savio Marques <savio.marques@sap.com>
2022-07-08 00:21:47 +0000

    APP Cultura

---
+++ app/cultura/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("agroproductdemo.app.cultura.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
