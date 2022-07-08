d495fa9a4916f822e17fc7e95d7fd1a5d889eff0
Savio Marques <savio.marques@sap.com>
2022-07-07 23:20:44 -0300

    ADJUSTMENTS

--- app/produtos/webapp/Component.js
+++ app/produtos/webapp/Component.js
@@ -3,7 +3,7 @@ sap.ui.define(
     function (Component) {
         "use strict";
 
-        return Component.extend("agroproductdemo.produtos.Component", {
+        return Component.extend("agroproductdemo.app.produtos.Component", {
             metadata: {
                 manifest: "json"
             }
