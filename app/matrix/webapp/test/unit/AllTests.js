9e439355907293677b2814574c6139797f3d1561
Savio Marques <savio.marques@sap.com>
2022-07-07 21:13:03 -0300

    App pragas

--- app/matrix/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"agroproductdemoapp/matrix/test/unit/controller/Matrix.controller"
-], function () {
-	"use strict";
-});
---
+++ app/pragas/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("agroproductdemo.app.pragas.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
