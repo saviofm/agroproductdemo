2b76f28735676d0861e8e635830ca60e3b96cfee
Savio Marques <savio.marques@sap.com>
2022-07-08 00:33:10 +0000

    App Principio Ativo

---
+++ app/principioativo/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("agroproductdemo.app.principioativo.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
